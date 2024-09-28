2dd6341a5fc527dfc0a210f7dae1e7fc420cd43d
21ow <kimjinewn@gmail.com>
2024-09-28 21:44:12 +0900

    feat: Import css files in main.jsx (#16)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Router from './Router';
+import './styles/global.css';
+import './styles/reset.css';
+import './styles/variables.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
