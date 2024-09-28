913976ec27636d03932685e0f56b6aa5c32d43ad
jsh1147 <jsh1147@naver.com>
2024-09-29 02:58:34 +0900

    feat: Add basic layout structure (#2)

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,13 +5,15 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import CreditTestPage from './pages/CreditTestPage';
+import Layout from './components/layout/Layout';
+import ListPage from './pages/listPage/ListPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index element={<h1>Rending Page</h1>} />
-      <Route>
-        <Route path="list" element={<h1>List Page</h1>} />
+      <Route element={<Layout />}>
+        <Route path="list" element={<ListPage />} />
         <Route path="mypage" element={<h1>My Page</h1>} />
         <Route path="creditTest" element={<CreditTestPage />} />
         <Route path="*" element={<h1>Non Page</h1>} />
---
+++ src/pages/listPage/ListPage.jsx
@@ -0,0 +1,11 @@
+import styles from './ListPage.module.css';
+
+export default function ListPage() {
+  return (
+    <main className={styles.main}>
+      <h1>Credit</h1>
+      <h1>Donate</h1>
+      <h1>Chart</h1>
+    </main>
+  );
+}
