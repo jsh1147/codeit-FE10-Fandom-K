41c0a4dbea2fa37e13f7edad264b4a256add3502
jsh1147 <jsh1147@naver.com>
2024-09-29 05:18:13 +0900

    feat: Implement Header component (#2)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
+import logo from '../../assets/images/logo.svg';
+import profile from '../../assets/images/user-profile.svg';
 
 export default function Header() {
-  return <header className={styles['header']}>Header</header>;
+  const { pathname: path } = useLocation();
+
+  const handleLinkClick = (e) => {
+    const targetPath = e.target.parentElement.pathname;
+    if (path === targetPath) window.location.reload();
+  };
+
+  return (
+    <header className={styles['header']}>
+      <Link to="/list" onClick={handleLinkClick}>
+        <img src={logo} alt="메인 페이지 바로가기" />
+      </Link>
+      <Link to="/mypage" onClick={handleLinkClick}>
+        <img src={profile} alt="마이 페이지 바로가기" />
+      </Link>
+    </header>
+  );
 }
