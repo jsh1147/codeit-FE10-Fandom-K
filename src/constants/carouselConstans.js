9211c14939ab7158578229b980ada59a1f163a2a
heejin <dlwlrgkwk82@gmail.com>
2024-10-02 01:36:01 +0900

    feat: Add donations carousel arrow setting

---
+++ src/constants/carouselConstans.js
@@ -0,0 +1,124 @@
+export const donationsSettings = {
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3.7,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 1120,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 1070,
+      settings: {
+        slidesToShow: 3.3,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 978,
+      settings: {
+        slidesToShow: 3.1,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 930,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 910,
+      settings: {
+        slidesToShow: 2.7,
+        slidesToScroll: 2,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 830,
+      settings: {
+        slidesToShow: 2.4,
+        slidesToScroll: 2,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4.2,
+        slidesToScroll: 4,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 738,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 3.1,
+        slidesToScroll: 3,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 2.7,
+        slidesToScroll: 2,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2.2,
+        slidesToScroll: 2,
+        arrows: false,
+        swipe: true,
+      },
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        swipe: true,
+      },
+    },
+  ],
+};
--- src/pages/listPage/Donation/Donations.jsx
+++ src/pages/listPage/Donation/Donations.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { donationsSettings as settings } from '@/constants/carouselConstans';
 
 import styles from './Donations.module.css';
 // import { getDonations } from '@/apis/donationsApi';
@@ -21,131 +22,6 @@ export default function Donations() {
 
     fetchItems();
   }, []);
-
-  const settings = {
-    speed: 500,
-    slidesToShow: 4, // Adjust this value as needed
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1199, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.7, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 1120, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.5, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 1070, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.3, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 978, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.1, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 930, // Mobile and smaller
-        settings: {
-          slidesToShow: 3, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 910, // Mobile and smaller
-        settings: {
-          slidesToShow: 2.7, // Show 1 card on smaller screens
-          slidesToScroll: 2,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 830, // Mobile and smaller
-        settings: {
-          slidesToShow: 2.4, // Show 1 card on smaller screens
-          slidesToScroll: 2,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 767, // Mobile and smaller
-        settings: {
-          slidesToShow: 4.2, // Show 1 card on smaller screens
-          slidesToScroll: 4,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 738, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.5, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 640, // Mobile and smaller
-        settings: {
-          slidesToShow: 3.1, // Show 1 card on smaller screens
-          slidesToScroll: 3,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 550, // Mobile and smaller
-        settings: {
-          slidesToShow: 2.7, // Show 1 card on smaller screens
-          slidesToScroll: 2,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 500, // Mobile and smaller
-        settings: {
-          slidesToShow: 2.2, // Show 1 card on smaller screens
-          slidesToScroll: 2,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-      {
-        breakpoint: 380, // Mobile and smaller
-        settings: {
-          slidesToShow: 2, // Show 1 card on smaller screens
-          slidesToScroll: 2,
-          arrows: false, // Disable arrows for responsive views
-          swipe: true, // Allow swipe scrolling
-        },
-      },
-    ],
-  };
 
   return (
     <section className={styles.donations}>
