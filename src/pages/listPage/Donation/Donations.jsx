88867080a5bd6116d7946f76fcde6f9d6019d592
heejin <dlwlrgkwk82@gmail.com>
2024-10-01 19:44:26 +0900

    feat: Add donation carousel

--- src/pages/listPage/Donation/Donations.jsx
+++ src/pages/listPage/Donation/Donations.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import styles from './Donations.module.css';
 // import { getDonations } from '@/apis/donationsApi';
@@ -19,11 +22,39 @@ export default function Donations() {
     fetchItems();
   }, []);
 
+  const settings = {
+    speed: 500,
+    slidesToShow: 4, // Adjust this value as needed
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1199, // Mobile and smaller
+        settings: {
+          slidesToShow: 3, // Show 1 card on smaller screens
+          slidesToScroll: 3,
+          arrows: false, // Disable arrows for responsive views
+          swipe: true, // Allow swipe scrolling
+        },
+      },
+      {
+        breakpoint: 767, // Mobile and smaller
+        settings: {
+          slidesToShow: 2, // Show 1 card on smaller screens
+          slidesToScroll: 2,
+          arrows: false, // Disable arrows for responsive views
+          swipe: true, // Allow swipe scrolling
+        },
+      },
+    ],
+  };
+
   return (
     <section className={styles.donations}>
-      {donations.map((item) => (
-        <DonationCard key={item.id} {...item} />
-      ))}
+      <Slider {...settings}>
+        {donations.map((item) => (
+          <DonationCard key={item.id} {...item} />
+        ))}
+      </Slider>
     </section>
   );
 }
