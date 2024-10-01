0f81c63b55122be9f92850ad2b40ad7e227f4a63
heejin <dlwlrgkwk82@gmail.com>
2024-10-01 13:23:49 +0900

    refactor: Move toastContainer, edit no-used-vars option

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import { ToastContainer } from 'react-toastify';
 
 export default function Layout() {
   return (
     <>
       <Header />
       <Outlet />
-      <ToastContainer autoClose={1500} closeButton={false} theme="dark" />
     </>
   );
 }
