29010b2bd30be21ec50a280ace3b526332a2ae2c
jsh1147 <jsh1147@naver.com>
2024-10-03 06:12:45 +0900

    feat: Add MyCredit functionality (#40)

--- src/pages/listPage/MyCredit/MyCredit.jsx
+++ src/pages/listPage/MyCredit/MyCredit.jsx
@@ -1,14 +1,19 @@
+import { useCredit } from '@/hooks/useCredit';
 import creditImage from '@/assets/icons/credit.svg';
 import styles from './MyCredit.module.css';
 
 export default function MyCredit() {
+  const { credit } = useCredit();
+
   return (
     <section className={styles.section}>
       <div className={styles.content}>
         <h2 className={styles.title}>내 크레딧</h2>
         <div className={styles.credit}>
           <img className={styles.image} src={creditImage} />
-          <span className={styles.amount}></span>
+          <span className={styles.amount}>
+            {Number(credit).toLocaleString()}
+          </span>
         </div>
       </div>
       <button className={styles.button}>충전하기</button>
