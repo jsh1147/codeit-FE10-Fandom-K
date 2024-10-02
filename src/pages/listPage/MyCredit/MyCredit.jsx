ed82ee203cb9259333455b541ec273cd068ed664
jsh1147 <jsh1147@naver.com>
2024-10-03 06:04:15 +0900

    feat: Add MyCredit CSS (#40)

--- src/pages/listPage/MyCredit/MyCredit.jsx
+++ src/pages/listPage/MyCredit/MyCredit.jsx
@@ -1,16 +1,17 @@
 import creditImage from '@/assets/icons/credit.svg';
+import styles from './MyCredit.module.css';
 
 export default function MyCredit() {
   return (
-    <section>
-      <div>
-        <span>내 크레딧</span>
-        <div>
-          <img src={creditImage} />
-          <span></span>
+    <section className={styles.section}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>내 크레딧</h2>
+        <div className={styles.credit}>
+          <img className={styles.image} src={creditImage} />
+          <span className={styles.amount}></span>
         </div>
       </div>
-      <button>충전하기</button>
+      <button className={styles.button}>충전하기</button>
     </section>
   );
 }
