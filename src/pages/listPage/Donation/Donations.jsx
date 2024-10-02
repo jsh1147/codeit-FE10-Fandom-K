85b11628fc9f4e53eff0bb6815a5e2e11f3b9b29
heejin <dlwlrgkwk82@gmail.com>
2024-10-03 01:01:58 +0900

    refactor: Add Spinners component and modify return on donations

--- src/pages/listPage/Donation/Donations.jsx
+++ src/pages/listPage/Donation/Donations.jsx
@@ -1,37 +1,37 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { donationsSettings as settings } from '@/constants/carouselConstants';
-import spinners from '@/assets/icons/spinners.svg';
-
 import styles from './Donations.module.css';
-import { getDonations } from '@/apis/donationsApi';
-
+import Slider from 'react-slick';
+import { donationsSettings as settings } from '@/constants/carouselConstants';
 import DonationCard from './components/DonationCard';
 import FetchError from './components/FetchError';
+import { getDonations } from '@/apis/donationsApi';
 import { useApiFetch } from '../../../hooks/useApiFetch';
+import Spinners from '../../../components/icons/Spinners';
 
 export default function Donations() {
   const { data, isLoading, error } = useApiFetch(getDonations, 20);
   const donations = data?.list || [];
 
-  return (
-    <>
-      <section className={styles.donations}>
-        {isLoading ? (
-          <div className={styles.spinnersWrapper}>
-            <img src={spinners} alt="로딩 아이콘" className={styles.spinners} />
-          </div>
-        ) : error ? (
-          <FetchError error={error} />
-        ) : (
-          <Slider {...settings}>
-            {donations.map((item) => (
-              <DonationCard key={item.id} {...item} />
-            ))}
-          </Slider>
-        )}
-      </section>
-    </>
-  );
+  const renderContent = () => {
+    if (isLoading) {
+      return (
+        <div className={styles.spinnersWrapper}>
+          <Spinners width="80px" height="80px" />
+        </div>
+      );
+    }
+
+    if (error) {
+      return <FetchError error={error} />;
+    }
+
+    return (
+      <Slider {...settings}>
+        {donations.map((item) => (
+          <DonationCard key={item.id} {...item} />
+        ))}
+      </Slider>
+    );
+  };
+
+  return <section className={styles.donations}>{renderContent()}</section>;
 }
