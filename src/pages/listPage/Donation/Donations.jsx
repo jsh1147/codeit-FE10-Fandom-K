2c2db24cc8686bd91cd4aeacc494ad36da3943e2
heejin <dlwlrgkwk82@gmail.com>
2024-10-02 18:11:40 +0900

    feat: Add donations spinners

--- src/pages/listPage/Donation/Donations.jsx
+++ src/pages/listPage/Donation/Donations.jsx
@@ -3,33 +3,42 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { donationsSettings as settings } from '@/constants/carouselConstants';
+import spinners from '@/assets/icons/spinners.svg';
 
 import styles from './Donations.module.css';
-// import { getDonations } from '@/apis/donationsApi';
-import { getDonations } from '@/apis/donationsMockApi';
+import { getDonations } from '@/apis/donationsApi';
 
 import DonationCard from './components/DonationCard';
 
 export default function Donations() {
   const [donations, setDonations] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
-      // const { list } = await getDonations();
-      const { list } = await getDonations({});
+      const { list } = await getDonations();
       setDonations(list);
+      setIsLoading(false);
     };
 
     fetchItems();
   }, []);
 
   return (
-    <section className={styles.donations}>
-      <Slider {...settings}>
-        {donations.map((item) => (
-          <DonationCard key={item.id} {...item} />
-        ))}
-      </Slider>
-    </section>
+    <>
+      <section className={styles.donations}>
+        {isLoading ? (
+          <div className={styles.spinnersWrapper}>
+            <img src={spinners} alt="로딩 아이콘" className={styles.spinners} />
+          </div>
+        ) : (
+          <Slider {...settings}>
+            {donations.map((item) => (
+              <DonationCard key={item.id} {...item} />
+            ))}
+          </Slider>
+        )}
+      </section>
+    </>
   );
 }
