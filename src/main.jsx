86527287d1d7bde7fadbd36b1dc9d41aec9a6ef9
21ow <kimjinewn@gmail.com>
2024-10-02 18:32:00 +0900

    Solve conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
-import Router from './Router';
-import './styles/global.css';
-import './styles/reset.css';
-import './styles/variables.css';
-=======
 import App from './App';
 import './styles/global.css';
 import './styles/reset.css';
@@ -14,7 +8,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './styles/libraryCustom.css';
->>>>>>> develop
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
