b34ed03249462b44f2cb584b66e8b0b00a5ab809
21ow <kimjinewn@gmail.com>
2024-10-02 15:52:50 +0900

    feat: Add chart component (#14)

---
+++ src/pages/Chart/Chart.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import Header from './header';
+import Button from './Button';
+import IdolCard from './IdolCard';
+import getChart from './Api';
+import styles from './Chart.module.css';
+
+export default function Chart() {
+  const [activeTab, setActiveTab] = useState('female');
+  const [idolList, setIdolList] = useState([]);
+  const pageSize = 10;
+
+  const handleChangeList = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const fetchIdolList = async ({ gender, cursor, pageSize }) => {
+    const idols = await getChart(gender, cursor, pageSize);
+    const sortedIdols = idols.sort((a, b) => b.totalVotes - a.totalVotes);
+    setIdolList(sortedIdols);
+  };
+
+  useEffect(() => {
+    fetchIdolList({
+      gender: activeTab,
+      cursor: null,
+      pageSize,
+    });
+  }, [activeTab, pageSize]);
+
+  return (
+    <section className={styles.chart}>
+      <Header />
+
+      <div className={styles.tabs}>
+        <Button
+          label="이달의 여자 아이돌"
+          status="female"
+          activeTab={activeTab}
+          onClick={() => handleChangeList('female')}
+        />
+        <Button
+          label="이달의 남자 아이돌"
+          status="male"
+          activeTab={activeTab}
+          onClick={() => handleChangeList('male')}
+        />
+      </div>
+
+      <ol className={styles.list}>
+        {idolList.map((idol, index) => (
+          <IdolCard idol={idol} index={index} key={idol.id} />
+        ))}
+      </ol>
+    </section>
+  );
+}
