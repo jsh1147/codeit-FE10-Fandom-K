43a150e103f0dc683edeea880eef722ddc31a4bc
21ow <kimjinewn@gmail.com>
2024-10-04 13:11:15 +0900

    feat: Add LoadMore component (#37)

--- src/pages/listPage/Chart/Chart.jsx
+++ src/pages/listPage/Chart/Chart.jsx
@@ -2,21 +2,34 @@ import { useEffect, useState } from 'react';
 import Title from './components/Title';
 import Tab from './components/Tab';
 import IdolCard from './components/IdolCard';
+import LoadMore from './components/LoadMore';
 import { getCharts } from '@/apis/chartsApi.js';
 import styles from './Chart.module.css';
 
 export default function Chart() {
   const [activeTab, setActiveTab] = useState('female');
   const [idolList, setIdolList] = useState([]);
+  const [cursor, setCursor] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
 
   const handleChangeList = (tab) => {
     setActiveTab(tab);
+    setIdolList([]);
+    setCursor(null);
+    setHasMore(true);
   };
 
-  const fetchIdolList = async ({ gender, cursor, pageSize }) => {
+  const fetchIdolList = async ({ gender, cursor, pageSize = 10 }) => {
     const data = await getCharts(gender, cursor, pageSize);
     const idols = data.idols;
-    setIdolList(idols);
+    const nextCursor = data.nextCursor;
+
+    if (idols.length < pageSize || !nextCursor) {
+      setHasMore(false);
+    }
+
+    setIdolList((prevList) => [...prevList, ...idols]);
+    setCursor(nextCursor);
   };
 
   useEffect(() => {
@@ -26,31 +39,39 @@ export default function Chart() {
     });
   }, [activeTab]);
 
+  const handleLoadMore = () => {
+    fetchIdolList({
+      gender: activeTab,
+      cursor,
+    });
+  };
+
   return (
     <section className={styles.chart}>
-      <Title />
-
-      <div className={styles.tabs}>
-        <Tab
-          label="이달의 여자 아이돌"
-          status="female"
-          activeTab={activeTab}
-          onClick={() => handleChangeList('female')}
-        />
-        <Tab
-          label="이달의 남자 아이돌"
-          status="male"
-          activeTab={activeTab}
-          onClick={() => handleChangeList('male')}
-        />
+      <div className={styles.content}>
+        <Title />
+        <div className={styles.tabs}>
+          <Tab
+            label="이달의 여자 아이돌"
+            status="female"
+            activeTab={activeTab}
+            onClick={() => handleChangeList('female')}
+          />
+          <Tab
+            label="이달의 남자 아이돌"
+            status="male"
+            activeTab={activeTab}
+            onClick={() => handleChangeList('male')}
+          />
+        </div>
+        <ol className={styles.list}>
+          {idolList &&
+            idolList.map((idol, index) => (
+              <IdolCard idol={idol} index={index} key={idol.id} />
+            ))}
+        </ol>
       </div>
-
-      <ol className={styles.list}>
-        {idolList &&
-          idolList.map((idol, index) => (
-            <IdolCard idol={idol} index={index} key={idol.id} />
-          ))}
-      </ol>
+      {hasMore && <LoadMore onClick={handleLoadMore} />}
     </section>
   );
 }
