d02e31cc623b4198be5dc6abce6c3559d6d2f31c
Your Name <you@example.com>
2024-10-08 00:52:16 +0900

    Add warningmoal

--- src/pages/listPage/Chart/components/WarningModal.jsx
+++ src/pages/listPage/Chart/components/WarningModal.jsx
@@ -1,29 +1,27 @@
-import styles from './WarningModalTest.module.css';
+import { useCredit } from '@/hooks/useCredit';
+import Modal from '@/components/Modal';
+import Button from '@/components/Button';
 import creditIcon from '@/assets/icons/credit.svg';
-import closeIcon from '@/assets/icons/close-modal.svg';
+import styles from './WarningModal.module.css';
 
 const WarningModalTest = ({ isVisible, onClose }) => {
-  if (!isVisible) return null;
+  const { credit } = useCredit();
+
+  if (!isVisible || credit >= 1000) return null;
 
   return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContainer}>
-        <button className={styles.closeButton} onClick={onClose}>
-          <img src={closeIcon} alt="닫기" className={styles.closeIcon} />
-        </button>
-
+    <Modal onClose={onClose}>
+      <div className={styles.content}>
         <img src={creditIcon} alt="크레딧 아이콘" className={styles.icon} />
-
         <p className={styles.messageText}>
           앗! 투표하기 위한 <span className={styles.highlight}>크레딧</span>이
-          부족해요
+          부족해요.
         </p>
-
-        <button className={styles.confirmButton} onClick={onClose}>
+        <Button className={styles.confirmButton} onClick={onClose}>
           확인
-        </button>
+        </Button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
