9730008c30d3ff262373ce39162314f4efc98cd4
21ow <kimjinewn@gmail.com>
2024-10-07 23:12:25 +0900

    Solve conflict

--- src/pages/listPage/Chart/components/Title.jsx
+++ src/pages/listPage/Chart/components/Title.jsx
@@ -7,7 +7,7 @@ export default function Title({ onClick, ...modalProps }) {
     <div className={styles.header}>
       <h2 className={styles.title}>이달의 차트</h2>
 
-      <Button className={styles.button}>
+      <Button className={styles.button} onClick={onClick} {...modalProps}>
         <img src={chartIcon} alt="" />
         차트 투표하기
       </Button>
