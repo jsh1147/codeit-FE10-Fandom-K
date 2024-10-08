69f6002dc12e6e8fc2030ac1fd3851c8f3fc421a
Your Name <you@example.com>
2024-10-08 18:55:09 +0900

    refactor: Add empty state in InterstIdol

--- src/pages/MyPage/InterestIdol/components/InterestIdol.jsx
+++ src/pages/MyPage/InterestIdol/components/InterestIdol.jsx
@@ -5,27 +5,34 @@ export default function InterestIdol({ idols, removeIdol }) {
   return (
     <div className={styles.container}>
       <h2>내가 관심 있는 아이돌</h2>
-      <div className={styles.idolList}>
-        {idols.map((idol) => (
-          <div key={idol.id} className={styles.idolCard}>
-            <div className={styles.cardContent}>
-              <img
-                src={idol.profilePicture}
-                alt={idol.name}
-                className={styles.image}
-              />
-              <button
-                className={styles.deleteIcon}
-                onClick={() => removeIdol(idol.id)}
-              >
-                <img src={DeselectIcon} alt="삭제" />
-              </button>
+
+      {idols.length === 0 ? (
+        <div className={styles.emptyState}>
+          <p>현재 관심 있는 아이돌이 없습니다.</p>
+        </div>
+      ) : (
+        <div className={styles.idolList}>
+          {idols.map((idol) => (
+            <div key={idol.id} className={styles.idolCard}>
+              <div className={styles.cardContent}>
+                <img
+                  src={idol.profilePicture}
+                  alt={idol.name}
+                  className={styles.image}
+                />
+                <button
+                  className={styles.deleteIcon}
+                  onClick={() => removeIdol(idol.id)}
+                >
+                  <img src={DeselectIcon} alt="삭제" />
+                </button>
+              </div>
+              <p className={styles.name}>{idol.name}</p>
+              <p className={styles.group}>{idol.group}</p>
             </div>
-            <p className={styles.name}>{idol.name}</p>
-            <p className={styles.group}>{idol.group}</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
